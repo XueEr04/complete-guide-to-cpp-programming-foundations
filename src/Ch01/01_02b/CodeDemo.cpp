19cf619aa157c6d0d6b652cc088b2e1cb00282e7
XueEr04 <xueer.ng.983@cranfield.ac.uk>
2026-01-20 08:10:13 +0000

    Comments

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -21,5 +21,14 @@ int main(){         //main entry point of our programme,
                     //which is enclosed by parentheses.
 
 //The body of the function is a block of code and blocks are delimited by curly brackets, symbolizing the beginning and end of the block.
+//using an object from the iostream file, which is a member of the standard library denoted by std. This membership is represented by the scope resolution operator, double colon. The object is called 'cout', which is short for character output. This represents the terminal and we must send some text to it. 
+//We do that with the left bound insertion operator or double less than characters. To the right of it, we enter whatever we what to send to the object at the left, and we may use the operator consecutively. Insert your message here. After that, I'll send a new line character, which is available by another standard library symbol 'endl', which is short for end line.
+
+    std::cout << "Hi there" << std::endl;
+    return 0;
+
+//We finish almost every line of code with a semicolon, but there are exceptions. For example, the lines that end in a closing curly bracket. 
+//Finally, since 'main()' is a function that returns an integer, we'll return zero with the 'return' keyword. 
+//The reason I returned zero is because zero is traditionally used to mean that no errors came in the program. Other values useally represent error codes, which you're expected todetail in your documentation. 
 }
 
