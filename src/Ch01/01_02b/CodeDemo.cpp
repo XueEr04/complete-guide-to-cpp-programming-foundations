a647fcbec4bd208a7dc989bd6cf066e1a07871c8
XueEr04 <xueer.ng.983@cranfield.ac.uk>
2026-01-20 07:26:02 +0000

    Comments

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -2,5 +2,24 @@
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
 
-#include <iostream>
+#include <iostream> //include the libraries we'll use
+                    //a pre-processor directive
+                    //<iostream> is a part of the C++ standard library
+                    //contains objects and functions to print text to the screen 
+                    //and to receive text from the keyboard
+                    //preprocessor directives do not end with a semicolon
+
+int main(){         //main entry point of our programme,
+                    //a function that is executed by default
+                    //'main' function must return an integer denoted by 
+                    //the 'int' keyword
+                    //eg. int <name of the function> <list of parameters>
+                    //the main function may receive either nothing or a set of strings
+                    //that come from the command line,
+                    //just the way you run command with arguments.
+                    //In this case, we'll use an empty list of parameters
+                    //which is enclosed by parentheses.
+
+//The body of the function is a block of code and blocks are delimited by curly brackets, symbolizing the beginning and end of the block.
+}
 
