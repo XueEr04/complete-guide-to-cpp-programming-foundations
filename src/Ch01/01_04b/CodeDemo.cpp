ad4e6f22531314e5a709fb14222bdc04561753e9
XueEr04 <xueer.ng.983@cranfield.ac.uk>
2026-01-21 13:23:50 +0000

    Notes

--- src/Ch01/01_04b/CodeDemo.cpp
+++ src/Ch01/01_04b/CodeDemo.cpp
@@ -9,8 +9,18 @@ int add(int a, int b){
     return a + b;
 }
 
+int doubleNumber(int num){
+    return num * 2;
+}
+
+void printMessage(){
+    std::cout << "Understand void return type: " << std::endl;
+}
+
 int main(){
     int result = add(5, 3);
+    int result02 = doubleNumber(5);
     std::cout << "The sum is: " << result << std::endl;
+    std::cout << "The double number is: " << result02 << std::endl;
     return 0;
 }
